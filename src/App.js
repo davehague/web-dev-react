7329ab6926467ebda240af0bd9149d159b18d6d0
David Hague <david.hague@gofmx.com>
2022-01-19 14:30:12 -0500

    Fetching names from API and using componentDidMount lifecycle method to populate

--- src/App.js
+++ src/App.js
@@ -6,34 +6,27 @@ class App extends Component {
     super(); // Gives access to app state
 
     this.state = {
-      monsters: [
-        {
-          id: 1,
-          name: 'Frankenstein'
-        },
-        {
-          id: 2,
-          name: 'Dracula'
-        },
-        {
-          id: 3,
-          name: 'Zombie'
-        }
-      ]
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(
-            // Need to provide a key, so react knows what to update if one of our elements changes
-            // and it doesn't need to re-render what hasn't changed
-            // Dev console will yell if you don't provide a key
-            monster => <h1 key={monster.id}>{monster.name}</h1>  
+        {this.state.monsters.map(
+          // Need to provide a key, so react knows what to update if one of our elements changes
+          // and it doesn't need to re-render what hasn't changed
+          // Dev console will yell if you don't provide a key
+          (monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
           )
-        }
+        )}
       </div>
     );
   }
