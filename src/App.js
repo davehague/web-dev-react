a76d1a93a4364778436a1f6e135ff49b2f2e8503
David Hague <david.hague@gofmx.com>
2022-01-19 12:05:48 -0500

    Working with our monsters state, and mapping to screen by providing ID as the key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,18 +6,34 @@ class App extends Component {
     super(); // Gives access to app state
 
     this.state = {
-      name: 'George'
-    }
+      monsters: [
+        {
+          id: 1,
+          name: 'Frankenstein'
+        },
+        {
+          id: 2,
+          name: 'Dracula'
+        },
+        {
+          id: 3,
+          name: 'Zombie'
+        }
+      ]
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name}</p>
-          <button onClick={() => this.setState({ name: 'Dave' })}>Click me to change the name!</button>
-        </header>
+        {
+          this.state.monsters.map(
+            // Need to provide a key, so react knows what to update if one of our elements changes
+            // and it doesn't need to re-render what hasn't changed
+            // Dev console will yell if you don't provide a key
+            monster => <h1 key={monster.id}>{monster.name}</h1>  
+          )
+        }
       </div>
     );
   }
